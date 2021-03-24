9d6ebc8cb9416e28e772cc2863f6605392f54a61
Dempsey <dempsey.rhian.neal@gmail.com>
2021-03-24 10:03:50 +0000

    pulling updates

--- src/components/Pages/Pages.js
+++ src/components/Pages/Pages.js
@@ -5,8 +5,9 @@ const Pages = () => {
 
     return (
         <>
-            {/* page-1 book cover and info ---------------------------------------------*/}
-            <div className="shadow-lg container" style= {
+            {/* page-1 book cover and info. PARENT DIV----------------------*/}
+            <div className="shadow-lg container" 
+                 style= {
                 {
                     backgroundColor: "#fdfaf7",
                     width: "500px",
@@ -19,11 +20,10 @@ const Pages = () => {
             }>
                 
                 <div>
-                <p className="header">PAGE HEADER - 1</p>
-                
-                <figure className="img--book">
-                    <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474169725l/15881._SY475_.jpg" alt="logo" class="logo"/>
-                </figure>
+                    <p className="header">PAGE HEADER - 1</p>
+                    <figure className="img--book">
+                        <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474169725l/15881._SY475_.jpg" alt="logo" class="logo"/>
+                    </figure>
                 </div>
 
                 <div style={
@@ -48,8 +48,9 @@ const Pages = () => {
             </div>
 
 
-            {/* page-2 book review  --------------------------------------------------*/}
-            <div className="shadow-lg container" style= {
+                {/* page-2 book review  --------------------------------------------------*/}
+            <div className="shadow-lg container" 
+                    style= {
                 {
                     backgroundColor: "#fdfaf7",
                     width: "500px",
@@ -57,42 +58,47 @@ const Pages = () => {
                 }
             }>
 
-                <p className="title" style={
-                    {
-                        fontSize: "25px",
-                    }
-                }>Harry Potter and the Chamber of Secrets</p>
-                
-
-                <p className="author-name" style={
+                    <p className="title" 
+                       style={
                         {
-                            color: colors.darkGrey,
+                            fontSize: "25px",
                         }
-                }>J K Rowling</p>
-                <p className="review">Review
-                I've yet to meet a ten-year-old who hasn't been entranced by its witty, complex plot and the character of the eponymous Harry ― Independent
+                    }>Harry Potter and the Chamber of Secrets</p>
 
-                Spellbinding, enchanting, bewitching stuff ― Mirror
+                    <p className="author-name" 
+                       style={
+                            {
+                                color: colors.darkGrey,
+                            }
+                    }>J K Rowling</p>
 
-                Teachers say a chapter can silence the most rowdy of classes ― Guardian
+                    <p className="review">Review
+                    I've yet to meet a ten-year-old who hasn't been entranced by its witty, complex plot and the character of the eponymous Harry ― Independent
 
-                One of the greatest literary adventures of modern times ― Sunday Telegraph
+                    Spellbinding, enchanting, bewitching stuff ― Mirror
 
-                The Harry Potter stories will join that small group of children's books which are read and reread into adulthood ― TLS
-                From the Back Cover
-                Harry Potter's summer has included the worst birthday ever, doomy warnings from a house-elf called Dobby, and rescue from the Dursleys by his friend Ron Weasley in a magical flying car! Back at Hogwarts School of Witchcraft and Wizardry for his second year, Harry hears strange whispers echo through empty corridors - and then the attacks start. Students are found as though turned to stone ...Dobby's sinister predictions seem to be coming true. These new editions of the classic and internationally bestselling, multi-award-winning series feature instantly pick-up-able new jackets by Jonny Duddle, with huge child appeal, to bring Harry Potter to the next generation of readers. It's time to PASS THE MAGIC ON ...</p>
-                <p className="lorem">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
+                    Teachers say a chapter can silence the most rowdy of classes ― Guardian
+
+                    One of the greatest literary adventures of modern times ― Sunday Telegraph
+
+                    The Harry Potter stories will join that small group of children's books which are read and reread into adulthood ― TLS
+                    From the Back Cover
+                    Harry Potter's summer has included the worst birthday ever, doomy warnings from a house-elf called Dobby, and rescue from the Dursleys by his friend Ron Weasley in a magical flying car! Back at Hogwarts School of Witchcraft and Wizardry for his second year, Harry hears strange whispers echo through empty corridors - and then the attacks start. Students are found as though turned to stone ...Dobby's sinister predictions seem to be coming true. These new editions of the classic and internationally bestselling, multi-award-winning series feature instantly pick-up-able new jackets by Jonny Duddle, with huge child appeal, to bring Harry Potter to the next generation of readers. It's time to PASS THE MAGIC ON ...</p>
+                    <p className="lorem">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                    </p>
              
-            </div>
+                </div>
 
-            {/* page-3 author cover ---------------------------------------------------*/}
-            <div className="container">
-            </div>
+                {/* page-3 author cover ---------------------------------------------------*/}
+                <div className="container">
 
-            {/* page-4 author bio --------------------------------------------------*/}
-            <div className="container">
-            </div>
+                    <img src="https://images-na.ssl-images-amazon.com/images/S/amzn-author-media-prod/fvh43djng407r1iq142ng0sk5f._SY200_.jpg"/>
+                </div>
+
+                {/* page-4 author bio --------------------------------------------------*/}
+                <div className="container">
+                </div>
+            
         </>
     )
 }
