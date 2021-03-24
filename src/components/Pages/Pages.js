159c56774f9cc0e1105e7667531dea2f0f1c56f8
Dempsey <dempsey.rhian.neal@gmail.com>
2021-03-24 12:13:09 +0000

    added author pages

--- src/components/Pages/Pages.js
+++ src/components/Pages/Pages.js
@@ -21,6 +21,8 @@ const Pages = () => {
                 
                 <div>
                     <p className="header">PAGE HEADER - 1</p>
+
+                    {/* page-3 book cover image*/}
                     <figure className="img--book">
                         <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474169725l/15881._SY475_.jpg" alt="logo" class="logo"/>
                     </figure>
@@ -87,16 +89,84 @@ const Pages = () => {
                     <p className="lorem">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                     </p>
              
-                </div>
+            </div>
 
-                {/* page-3 author cover ---------------------------------------------------*/}
-                <div className="container">
 
-                    <img src="https://images-na.ssl-images-amazon.com/images/S/amzn-author-media-prod/fvh43djng407r1iq142ng0sk5f._SY200_.jpg"/>
-                </div>
+                {/* page-3 author cover ---------------------------------------------------------------------------------*/}
+            <div className="shadow-lg container"
+                 style={
+                        {
+                        backgroundColor: "#fdfaf7",
+                        width: "500px",
+                        height: "800px",
+                        }
+                 }>
+                    <div style={
+                            {
+                                display: "flex",
+                            }
+                    }>
+                        {/* page-3 author image*/}
+                        <img src="https://images-na.ssl-images-amazon.com/images/S/amzn-author-media-prod/fvh43djng407r1iq142ng0sk5f._SY200_.jpg" 
+                            style={
+                                {
+                                    height: "270px",
+                                    width: "250px",
+                                    marginTop: "15px"
+                                }    
+                        }/>
 
+                        <p className="" 
+                           style={
+                            {
+                                fontSize: "40px",
+                                marginTop: "50px",
+                                marginLeft: "20px"
+                            }
+                            }>J.K. Rowling</p>
+                    </div>    
+                   
+
+                    <p className="about-info" 
+                       style={
+                        {
+                            marginTop: "20px",
+                            marginLeft: "5px"
+                        }
+                    }>About the Author
+                    J.K. Rowling is best known as the author of the seven Harry Potter books, which were published between 1997 and 2007. The enduringly popular adventures of Harry, Ron and Hermione have gone on to sell over 500 million copies, be translated into over 80 languages and made into eight blockbuster films. Alongside the Harry Potter series, she also wrote three short companion volumes for charity: Quidditch Through the Ages and Fantastic Beasts and Where to Find Them, in aid of Comic Relief and Lumos, and The Tales of Beedle the Bard, in aid of Lumos. J.K. Rowling collaborated with playwright Jack Thorne and director John Tiffany to continue Harry's story in a stage play, Harry Potter and the Cursed Child, which opened in London in 2016 and is now playing worldwide. In the same year, she made her debut as a screenwriter with the film Fantastic Beasts and Where to Find Them, the first in a series featuring Magizoologist Newt Scamander, which was inspired by the original companion volume. J.K.</p>
+                
+            </div> 
                 {/* page-4 author bio --------------------------------------------------*/}
-                <div className="container">
+                <div className="shadow-lg container"
+                 style={
+                        {
+                        backgroundColor: "#fdfaf7",
+                        width: "500px",
+                        height: "800px",
+                        }
+                 }>
+                     <p className="author-name" 
+                       style={
+                            {
+                                color: colors.darkGrey,
+                            }
+                    }>Biography</p>
+
+                    <p className="bio">J.K. Rowling is best-known as the author of the seven Harry Potter books, which were published between 1997 and 2007. The enduringly popular adventures of Harry, Ron and Hermione have gone on to sell over 500 million copies, be translated into over 80 languages and made into eight blockbuster films.
+
+                    Alongside the Harry Potter series, J.K. Rowling also wrote three short companion volumes for charity: Quidditch Through the Ages and Fantastic Beasts and Where to Find Them, in aid of Comic Relief, and The Tales of Beedle the Bard, in aid of Lumos. The companion books and original series are all available as audiobooks.
+
+                    In 2016, J.K. Rowling collaborated with playwright Jack Thorne and director John Tiffany to continue Harry’s story in a stage play, Harry Potter and the Cursed Child, which opened in London, followed by the USA and Australia.
+
+                    In the same year, she made her debut as a screenwriter with the film Fantastic Beasts and Where to Find Them. Inspired by the original companion volume, it was the first in a series of new adventures featuring wizarding world magizoologist Newt Scamander. The second, Fantastic Beasts: The Crimes of Grindelwald, was released in 2018.
+
+                    Both the screenplays, as well as the script of Harry Potter and the Cursed Child, are also available as books.
+
+                    Fans of Fantastic Beasts and Harry Potter can find out more at www.wizardingworld.com.
+
+                    J.K. Rowling also writes novels for adults. The Casual Vacancy was published in 2012 and adapted for television in 2015. Under the pseudonym Robert Galbraith, she is the author of the highly acclaimed ‘Strike’ crime series, featuring private detective Cormoran Strike and his partner Robin Ellacott. The first of these, The Cuckoo’s Calling, was published to critical acclaim in 2013, at first without its author’s true identity being known. The Silkworm followed in 2014, Career of Evil in 2015 and Lethal White in 2018. 
+                    </p>
                 </div>
             
         </>
